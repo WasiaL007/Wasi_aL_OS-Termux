ab455959e34b05dc4f6f6a64afe3c97556475a1c
WasiaL007 <wasialaiub21@gmail.com>
2026-07-12 03:37:29 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,10 @@
 
 echo "Installing WASI AL OS..."
 
-echo "Download WASI AL OS APK from Releases"
+pkg update -y
+pkg install wget -y
+
+wget https://github.com/WasiaL007/WasiaL007/releases/download/v1.5/WASI_AL_OS_Termux_v1.5.apk
+
+echo "Download Complete!"
+echo "Open the APK file and install WASI AL OS."
